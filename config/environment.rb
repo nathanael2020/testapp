b0ed3792bbf87fade4d1de26a8fad018f398510e
Nathanael Miksis <nathanael.miksis@gmail.com>
2015-11-01 18:24:25 -0800

    remote test

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+export POSTGRES_USERNAME='nathanael'
+export POSTGRES_PASSWORD='password123'
